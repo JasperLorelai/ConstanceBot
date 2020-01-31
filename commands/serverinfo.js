27a802c3f488edea7f46dac938780dea91674fbd
IntricateDream <wolf.haci12@gmail.com>
2020-01-31 23:23:12 +0100

    Added Guild moderation verification constants in place of indexes

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -9,7 +9,17 @@ module.exports = {
         const {channels} = guild;
         const tc = channels.array().filter(c => c.type === "text");
         const vc = channels.array().filter(c => c.type === "voice");
-        const desc = "**Name:** " + guild.name + " (" + guild.nameAcronym + ")" + "\n**Guild ID:** `" + guild.id + "`" + "\n**Created at:** `" + guild.createdAt.toLocaleString() + "`" + "\n**Member count:** " + guild.memberCount + "\n**Guild owner:** " + guild.owner.toString() + "\n**Region:** " + guild.region + "\n**Default Message Notifications:** " + guild.defaultMessageNotifications.toFormalCase() + "\n**Moderation Verification Level:** " + guild.mfaLevel + (guild.features.length > 0 ? "\n**Features:** " + guild.features.map(f => "`" + f.toFormalCase() + "`").join(", ") : "") + (guild.premiumTier ? "\n**Boost Level:** Tier " + guild.premiumTier + " (boosts: " + guild.premiumSubscriptionCount + ")" : "") + (guild.verified ? "\n**Verified:** " + guild.verified : "") + (guild.partnered ? "\n**Partnered:** " + guild.partnered : "") + (tc.length ? "\n**Text Channels:** " + tc.length : "") + (vc.length ? "\n**Voice Channels:** " + vc.length : "");
+        // This might need to be updated whenever a new one is added. Alternatively,
+        // if it has to be, look into accessing the original array from
+        // discord.js: https://github.com/discordjs/discord.js/blob/master/src/util/Constants.js
+        const verificationLevels = [
+            "None",
+            "Low",
+            "Medium",
+            "(╯°□°）╯︵ ┻━┻",
+            "┻━┻ ﾐヽ(ಠ益ಠ)ノ彡┻━┻",
+        ];
+        const desc = "**Name:** " + guild.name + " (" + guild.nameAcronym + ")" + "\n**Guild ID:** `" + guild.id + "`" + "\n**Created at:** `" + guild.createdAt.toLocaleString() + "`" + "\n**Member count:** " + guild.memberCount + "\n**Guild owner:** " + guild.owner.toString() + "\n**Region:** " + guild.region + "\n**Default Message Notifications:** " + guild.defaultMessageNotifications.toFormalCase() + "\n**Moderation Verification Level:** " + verificationLevels[guild.verificationLevel] + (guild.features.length > 0 ? "\n**Features:** " + guild.features.map(f => "`" + f.toFormalCase() + "`").join(", ") : "") + (guild.premiumTier ? "\n**Boost Level:** Tier " + guild.premiumTier + " (boosts: " + guild.premiumSubscriptionCount + ")" : "") + (guild.verified ? "\n**Verified:** " + guild.verified : "") + (guild.partnered ? "\n**Partnered:** " + guild.partnered : "") + (tc.length ? "\n**Text Channels:** " + tc.length : "") + (vc.length ? "\n**Voice Channels:** " + vc.length : "");
         await channel.send(author.toString(), config.embed("Guild Info", desc).setThumbnail(guild.iconURL()));
     }
 };
