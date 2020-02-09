e493d76a2a65e8ebc7d2906b63369460f94e0596
IntricateDream <wolf.haci12@gmail.com>
2020-02-09 21:30:37 +0100

    Fix for Heroku web hosting

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => {
+    console.log("App running on port " + PORT);
+});
+
 // Add a handler for all application routes.
 app.get("/", (request, response) => client.app.get("defaultRoute")(request, response));
 app.get("/:route", (request, response) => {
