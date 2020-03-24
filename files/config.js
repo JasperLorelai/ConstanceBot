6852226931b7ea089aa95e3e899386db52945140
IntricateDream <wolf.haci12@gmail.com>
2020-03-24 18:21:55 +0100

    Moved staff apps in form list

--- files/config.js
+++ files/config.js
@@ -141,11 +141,11 @@ module.exports = {
                 "**IP:** `" + this.defaultIP + "`\n" +
                 "**Version:** Release 1.13.2\n" +
                 "**Discord Invite:** http://mhaprodigy.uk/discord\n" +
-                "**Trello Board:** http://mhaprodigy.uk/trello\n" +
-                "**Staff Apps:** http://mhaprodigy.uk/apply")
+                "**Trello Board:** http://mhaprodigy.uk/trello")
             .addField("Forms",
                 "**Would you like suggest something?** [(Click here)](http://mhaprodigy.uk/suggest)\n" +
-                "**Need support?** [(Click here)](http://mhaprodigy.uk/support)")
+                "**Need support?** [(Click here)](http://mhaprodigy.uk/support)" +
+                "**Would you like to apply for staff?** [(Click here)](http://mhaprodigy.uk/apply)")
             .addField("Authentication","🔞 - **NSFW Authentication** - *This authentication is here to warn the recipient to not open the channel at the office or near young children because the channels it will reveal after accepting contain sexual or repulsive content.*\nReact with the displayed reaction to toggle access to the NSFW channel section.")
             .addField("Toggle Pingable Roles",
                 "📦 - <@&" + this.roles.polls + "> - If you have this role, you will be pinged whenever a poll is up.\n" +
