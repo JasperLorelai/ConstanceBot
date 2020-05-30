3e4294e9a5958d4280ec3f6f7180caa46ee92793
JasperLorelai <wolf.haci12@gmail.com>
2020-05-30 15:34:49 +0200

    Added compatibility for more invites

--- commands/getinvite.js
+++ commands/getinvite.js
@@ -2,8 +2,21 @@ module.exports = {
     name: "getinvite",
     description: "Fetches the Discord invite.",
     guildOnly: true,
-    guildWhitelist: [require("../files/config").guilds.mhapGuild],
     async execute(message) {
-        await message.channel.send(message.author.toString() + "\nhttp://mhaprodigy.uk/discord");
+        const {guild, client} = message;
+        const {config, util} = client;
+        let invite;
+        switch (guild.id) {
+            case config.guilds.mhapGuild:
+                invite = "http://mhaprodigy.uk/discord";
+                break;
+            case config.guilds.nlGuild:
+                invite = "https://discord.gg/Z9R4j7g";
+                break;
+            default:
+                await message.channel.send(message.author.toString(), util.embed("Invite Fetch", "No saved invite was configured for this guild.", config.color.red));
+                return;
+        }
+        await message.channel.send(message.author.toString() + "\n" + invite);
     }
 };
