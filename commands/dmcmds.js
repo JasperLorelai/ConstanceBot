0f8428c165f33c9161075946918b9528a6ea6b51
JasperLorelai <wolf.haci12@gmail.com>
2020-06-05 23:36:52 +0200

    Filter bugfix

--- commands/dmcmds.js
+++ commands/dmcmds.js
@@ -3,12 +3,11 @@ module.exports = {
     description: "Lists of all commands that can be executed in DMs.",
     aliases: ["dmcommands"],
     async execute(message) {
-        const {client, channel, author, guild} = message;
+        const {client, channel, author} = message;
         const {commands, util} = client;
-        const text = commands
-            .filter(c => !c.guildOnly && c.hide && !(c.guildWhitelist && !c.guildWhitelist.includes(guild.id)))
-            .map(c => "\* `" + c.name + (c.params ? " " + c.params.join(" ") : "") + "` " + (c.perm && c.perm === "author" ? " (**Bot Author**)" : ""))
-            .join("\n");
+        let text = commands.filter(c => !c.guildOnly && !c.hide);
+        if (message.guild) text = text.filter(c => !(c.guildWhitelist && !c.guildWhitelist.includes(message.guild.id)));
+        text = text.map(c => "\* `" + c.name + (c.params ? " " + c.params.join(" ") : "") + "` " + (c.perm && c.perm === "author" ? " (**Bot Author**)" : "")).join("\n");
         await channel.send(author.toString(), util.embed("DM Command List", text));
     }
 };
