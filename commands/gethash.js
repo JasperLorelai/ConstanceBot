1a089fa63129047a6c89cba94993b74a1d8a3c34
JasperLorelai <wolf.haci12@gmail.com>
2020-06-17 01:05:05 +0200

    Repurposed "gethash" command

--- commands/gethash.js
+++ commands/gethash.js
@@ -1,13 +1,28 @@
 // noinspection JSUnusedLocalSymbols
 module.exports = {
     name: "gethash",
-    description: "Generates random MD5 hash.",
+    description: "Generates Sha1 checksum of the provided file.",
     guildOnly: false,
     async execute(message) {
         const {client, author, channel} = message;
-        const {util} = client;
+        const {util, config} = client;
         try {
-            channel.send(author.toString(), util.embed("Random Sha1 Generated", "`" + client.sha1(new Date()) + "`"));
+            const pack = message.attachments ? message.attachments.find(a => a.name.endsWith(".zip")) : null;
+            if (!pack) {
+                channel.send(author.toString(), util.embed("Sha1 Checksum Failed", "No attachment provided (must end with `zip` file extension).", config.color.red));
+                return;
+            }
+            const buffer = await client.fetch(pack.url).then(y => y.buffer());
+            message.delete({reason: "botIntent"});
+            const hash = client.sha1(buffer);
+            channel.send({
+                embed: util.embed("Sha1 Checksum Generated", "`" + hash + "`"),
+                content: author.toString(),
+                files: [{
+                    attachment: buffer,
+                    name: hash + ".zip"
+                }]
+            });
         }
         catch(e) {
             await util.handleError(message, e);
