422a7ae53a3c2ef190788e6fea45d60f0d0bfb3a
JasperLorelai <wolf.haci12@gmail.com>
2020-10-25 00:50:02 +0200

    Fix for message update event length error

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -12,12 +12,18 @@ client.on("messageUpdate", async (oldMessage, newMessage) => {
         ![config.guildData.mhap.categories.olympus, config.guildData.mhap.categories.archive].includes(channel["parentID"]) &&
         ![config.guildData.main.bot, nl.triumvirate, nl.leadership, nl.interview2, nl.interview].includes(channel.id)
     ) {
+        let oldContent = oldMessage ? oldMessage.content : "Error: The old content wasn't recorded because it was from a previous session of the bot.";
+        if (oldContent.length > 1024) oldContent = "Error: The old content is too long to display.";
+
+        let newContent = newMessage.content || "No content.";
+        if (newContent.length > 1024) newContent = "Error: The new content is too long to display.";
+
         util.log(guild, embed => embed.setColor(config.color.logs.messageUpdate)
             .setAuthor("@" + author.tag, author.displayAvatarURL())
             .setTitle("Message Edited")
             .setDescription(channel.toString() + " [\(Jump\)](" + newMessage.url + ")")
-            .addField("Before", oldMessage ? oldMessage.content : "Error: The old content wasn't recorded because it was from a previous session of the bot.", false)
-            .addField("Now", newMessage.content || "No content.", false)
+            .addField("Before", oldContent, false)
+            .addField("Now", newContent, false)
             .setFooter("Message ID: " + newMessage.id));
     }
 
