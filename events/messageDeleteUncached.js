3378a244cba3a0af47441b902c06fc6106927617
JasperLorelai <wolf.haci12@gmail.com>
2020-10-25 00:37:02 +0200

    Bugfix

--- events/messageDeleteUncached.js
+++ events/messageDeleteUncached.js
@@ -2,22 +2,21 @@ const client = require("../bot");
 client.on("messageDeleteUncached", async event => {
     const {config, util} = client;
     const guild = client["guilds"].resolve(event["guild_id"]);
-    const channelID = event["channel_id"];
+    const channel = guild.channels.resolve(event["channel_id"]);
     const messageID = event["id"];
 
     const nl = config.guildData.nl.channels;
     // Handle blacklists.
-    if (![config.guildData.mhap.categories.olympus, config.guildData.mhap.categories.archive].includes(channel["parentID"]) &&
+    if (![config.guildData.mhap.categories.olympus, config.guildData.mhap.categories.archive].includes(channel.parentID) &&
         ![config.guildData.main.channels.bot, nl.triumvirate, nl.leadership, nl.interview2, nl.interview].includes(channel.id)
     ) {
-        const channel = client["channels"].resolve(channelID);
         util.log(guild, embed => {
             embed = embed.setColor(config.color.logs.messageDelete)
                 .setTitle("Message Deleted")
                 .setDescription("**Uncached message deleted.**")
                 .setFooter("Message ID: " + messageID);
-            if (channel) embed = embed.setDescription(embed.description + "\n" + channel);
-            else embed = embed.setFooter(embed.footer + " | Channel ID: " + channelID)
+            if (channel) embed = embed.setDescription(embed.description + "\n" + channel.toString());
+            else embed = embed.setFooter(embed.footer + " | Channel ID: " + channel.id)
             return embed;
         });
     }
