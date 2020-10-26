50a05f1b4d09a7dd238de5475cf05257f377d12e
JasperLorelai <wolf.haci12@gmail.com>
2020-10-26 14:25:56 +0100

    Cleanup

--- files/prototype.js
+++ files/prototype.js
@@ -1,10 +1,10 @@
 module.exports = (Discord) => {
     String.prototype.toFormalCase = function() {
         return this.charAt(0).toUpperCase() + this.substr(1).toLowerCase();
-    };
+    }
     String.prototype.toTitleCase = function() {
         return this.split(" ").map(e => e.toFormalCase()).join(" ");
-    };
+    }
     String.prototype.discordMKD = function() {
         let splits = this.split("\n").filter(l => l !== "---");
         for (let i = 0; i < splits.length; i++) {
@@ -14,14 +14,14 @@ module.exports = (Discord) => {
             if (splits[i].startsWith("#")) splits[i] = "__**" + splits[i].substr(1) + "**__";
         }
         return splits.join("\n");
-    };
+    }
     String.prototype.escapeMarkdown = function() {
         return Discord.Util.escapeMarkdown(this);
     }
     Date.prototype.toLocalFormat = function() {
         return this.getDate() + "/" + (this.getMonth() + 1) + "/" + this.getFullYear() + ", " + this.getHours() + ":" + this.getMinutes() + ":" + this.getSeconds();
-    };
+    }
     Discord.MessageEmbed.prototype.setColorRandom = function() {
         return this.setColor(Math.floor(Math.random()*16777215));
-    };
+    }
 };
