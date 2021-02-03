a12221920a96eb1f17044c93a30ed91545ff4d04
JasperLorelai <wolf.haci12@gmail.com>
2021-02-03 20:03:25 +0100

    Fixed search, paginated

--- commands/Quirk.js
+++ commands/Quirk.js
@@ -8,25 +8,29 @@ module.exports = {
         const {Config, Util, ConditionException} = Libs;
         const {channel, author} = message;
 
-        if (args.length) {
-            const lists = await Util.getTrello("boards/" + Config.trello.boards.mhap + "/lists");
-            const quirkList = lists.find(l => l.name === "Quirks");
-            if (!quirkList) {
-                Config.botLog().send(Config.author.toString(), Util.embed("Quirk Command Exception", "User **" + author.username + "** couldn't request quirk information because the \"Quirks\" list couldn't be found. [\(Jump\)](" + message.url + ")", Config.color.red));
-                throw new ConditionException(author, "Quirks", "Exception encountered. This was automatically reported and will be resolved.");
-            }
-            let quirks = [];
-
-            for (let q of await Util.getTrello("lists/" + quirkList.id + "/cards")) {
-                quirks.push({id: q.id, desc: q["desc"], name: q["desc"].match(/##\s?Quirk:\s[^\n]*/g)[0].substr(10)});
-            }
-            const quirk = quirks.find(q => q.name.toLowerCase().includes(args.join(" ").toLowerCase()));
-            if (!quirk) throw new ConditionException(author, "Quirk " + args.join(" ").toFormalCase(), "Quirk not found. Please look through the list using the `quirks` command.");
-
-            channel.send(author.toString(), Util.embed("Quirk - " + quirk.name, quirk.desc.discordMKD()).setURL("https://trello.com/c/" + quirk.id));
+        if (!args.length) {
+            channel.send(author.toString(), Util.embed("Quirks", (await Util.getTrello("cards/" + Config.trello.cards.quirksRoster)).desc.discordMKD()).setURL("https://trello.com/c/" + Config.trello.cards.quirksRoster));
             return;
         }
 
-        channel.send(author.toString(), Util.embed("Quirks", (await Util.getTrello("cards/" + Config.trello.cards.quirksRoster)).desc.discordMKD()).setURL("https://trello.com/c/" + Config.trello.cards.quirksRoster));
+        const initialMsg = await channel.send(Util.embed("Quirks", "Pending information...", Config.color.yellow));
+
+        const lists = await Util.getTrello("boards/" + Config.trello.boards.mhap + "/lists");
+        const quirkList = lists.find(l => l.name === "Quirks");
+        if (!quirkList) {
+            Config.botLog().send(Config.author.toString(), Util.embed("Quirk Command Exception", "User **" + author.username + "** couldn't request quirk information because the \"Quirks\" list couldn't be found. [\(Jump\)](" + message.url + ")", Config.color.red));
+            initialMsg.delete();
+            throw new ConditionException(author, "Quirks", "Exception encountered. This was automatically reported and will be resolved.");
+        }
+
+        const quirks = await Util.getTrello("lists/" + quirkList.id + "/cards");
+        const quirkName = args.join(" ").toLowerCase();
+        const quirk = quirks.find(q => q.name.toLowerCase().includes(quirkName));
+        initialMsg.delete();
+        if (!quirk) throw new ConditionException(author, "Quirk " + quirkName.toFormalCase(), "Quirk not found. Please look through the list using the `quirks` command.");
+
+        const text = quirk.desc.discordMKD();
+        const msg = await channel.send(author.toString(), Util.embed("Quirk - " + quirk.name, (text.length >= 2000 ? "" : text)).setURL("https://trello.com/c/" + quirk.id));
+        if (text.length >= 2000) await Util.handlePrompt(msg, text);
     }
 };
