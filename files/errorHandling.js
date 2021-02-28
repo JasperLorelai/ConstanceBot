88f4cd1b55efa2ae0e5996261d68cbdb1594a041
JasperLorelai <wolf.haci12@gmail.com>
2021-02-28 16:32:34 +0100

    Cleanup

--- files/errorHandling.js
+++ files/errorHandling.js
@@ -2,20 +2,20 @@ const {fetch, Discord, Config} = require("../Libs");
 const {MessageEmbed} = Discord;
 
 async function handle(error, code = 0) {
-    if (error instanceof Error) {
-        const embed = new MessageEmbed()
-            .setTitle(error.message)
-            .setDescription(">>> " + error.stack)
-            .setColor(Config.color.red)
-            .toJSON();
-        await fetch(process.env.WEBHOOK_ERROR, {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({embeds: [embed]})
-        });
-    }
-
     setTimeout(() => process.exit(code), 1000);
+
+    // Send msg to webhook log.
+    if (!(error instanceof Error)) return;
+    const embed = new MessageEmbed()
+        .setTitle(error.message)
+        .setDescription(">>> " + error.stack)
+        .setColor(Config.color.red)
+        .toJSON();
+    await fetch(process.env.WEBHOOK_ERROR, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({embeds: [embed]})
+    });
 }
 
 process.on("uncaughtException", async error => await handle(error, 1));
